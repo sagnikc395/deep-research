eb3942a09eb5cdb7a1bec9c9e047f38cb72e575b
Sagnik Chatterjee <sagnikchatterjee607@gmail.com>
2026-03-19 13:58:55 -0400

    feat: update config

--- deep-research/config.py
+++ deep-research/config.py
@@ -10,21 +10,5 @@ with open("../deep-research-config.toml", "rb") as f:
 
 ## access the values
 app_name = config["app"]["name"]
-input_path = config["files"]["input_path"]
-db_port = config["database"]["port"]
-
-"""
-writing to the toml file 
-import tomli_w
-
-config = {
-    "app": {"name": "MyApp", "debug": True},
-    "files": {
-        "input_path": "data/input.csv",
-        "output_path": "data/output.csv",
-    }
-}
-
-with open("config.toml", "wb") as f:  # must be "wb"
-    tomli_w.dump(config, f)
-"""
+model_id = config["app"]["model_id"]
+model_provider = config["app"]["model_provider"]
